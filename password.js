d61c6415c334ff8b0f47353fbdf0602f4b747825
Krishel Lasam <krishel.lasam@gmail.com>
2018-06-01 19:45:57 +0000

    automatically backed up by learn

--- password.js
+++ password.js
@@ -8,8 +8,7 @@ var generatePassword = function generatePassword(length, charset) {
   return password
 }
 
-// var length = 10,
-var number = 1
+var count = 1
 var argv = process.argv
 
 if (argv[2]) {
